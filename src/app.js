5c6f686ed9e43438b3ec3b1ae97ff60fe32086b0
AnaMCS26 <144897504+AnaMCS26@users.noreply.github.com>
2023-11-27 20:44:40 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying",
+];
+
+function excuseGenerator(who, action, what, when) {
+  var numwho = Math.floor(Math.random() * who.length);
+  var numaction = Math.floor(Math.random() * action.length);
+  var numwhat = Math.floor(Math.random() * what.length);
+  var numwhen = Math.floor(Math.random() * when.length);
+
+  return (
+    who[numwho] +
+    " " +
+    action[numaction] +
+    " " +
+    what[numwhat] +
+    " " +
+    when[numwhen]
+  );
+}
+
+function onLoad() {
+  var excuse = document.getElementById("excuse");
+  excuse.innerHTML = excuseGenerator(who, action, what, when);
+}
+window.onload = onLoad;
