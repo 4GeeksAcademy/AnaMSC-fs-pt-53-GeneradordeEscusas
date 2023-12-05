30cf45e1117719ed6c0558a91ba11c27e4ed18a3
AnaMCS26 <144897504+AnaMCS26@users.noreply.github.com>
2023-12-05 13:09:31 +0000

    “unomas”

--- src/app.js
+++ src/app.js
@@ -5,32 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let Who = ["The dog", "My grandma", "His turtle", "My bird"];
-let Action = ["ate", "peed", "crushed", "broke"];
-let What = ["my homework", "the keys", "the car"];
-let hen = [
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
   "before the class",
   "right on time",
   "when I finished",
   "during my lunch",
-  "while I was praying"
+  "while I was praying",
 ];
 
 function excuseGenerator(who, action, what, when) {
-  var numwho = Math.floor(Math.random() * who.length);
-  var numaction = Math.floor(Math.random() * action.length);
-  var numwhat = Math.floor(Math.random() * what.length);
-  var numwhen = Math.floor(Math.random() * when.length);
+  var numWho = Math.floor(Math.random() * who.length);
+  var numAction = Math.floor(Math.random() * action.length);
+  var numWhat = Math.floor(Math.random() * what.length);
+  var numWhen = Math.floor(Math.random() * when.length);
 
-  return (
-    who[numwho] +
-    " " +
-    action[numaction] +
-    " " +
-    what[numwhat] +
-    " " +
-    when[numwhen]
-  );
+  return `${who[numWho]} ${action[numAction]} ${what[numWhat]} ${when[numWhen]}`;
 }
 
 function onLoad() {
